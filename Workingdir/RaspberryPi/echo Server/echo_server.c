1ec5e6d9b9353628cae5c687330325e44c98eb92
DESKTOP-6GC8TCI\PC <swlee1014@gmail.com>
2017-11-04 17:19:43 +0900

    FileTest

--- Workingdir/RaspberryPi/echo Server/echo_server.c
+++ Workingdir/RaspberryPi/echo Server/echo_server.c
@@ -113,18 +113,43 @@ void *listening_thread(void *arg)
 void *client_thread(void *arg)
 {
 	pthread_detach(pthread_self());
-
 	int clnt_sock;
 	clnt_sock = *(int *)arg;
-
 	char rcv_buf[RX_DATA_MAX];
-	int readBufSize,writeBufSize;
-	int i, j;
-	
+	int readBufSize,writeBufSize;	
 	unsigned char sendingErr = 0;
 
+	char filename[20] = "filetest0.txt";		//파일 이름 -------------------수정 v
+
+	FILE* stream = fopen(filename, "w");		//파일 오픈 
+	if(stream == NULL)							//오픈 확인 
+	{
+		printf("file open error \n");
+		exit(1);
+	}										//=------------------------수정 ^
+	
 	while(1)
 	{
+		if(ftell(stream) > 1000)				//각 파일의 크기 검사 -----------수정 v
+		{
+			printf("new file opened \n");	
+			if(fclose(stream) == EOF)			//크기를 벗어나면 파일닫음 
+			{
+				printf("file close error \n");
+				exit(1);
+			}
+			
+			filename[8]++;						//새로운 파일을 열기위한 단계 
+			
+			stream = fopen(filename, "w");		//새 파일 오픈 
+			
+			if(stream ==NULL)		//오픈 확인 
+			{
+				printf("file open error \n");
+				exit(1);
+			} 
+		}							//------------------수정 ^
+		
 		readBufSize = read(clnt_sock, rcv_buf, RX_DATA_MAX);  //소켓이로부터 문자를 읽어서 rcv_buf에 저장 하는데 그 크기는  최대 크기는 2048BYTE다 
 
 		if(readBufSize > 0)
@@ -132,7 +157,9 @@ void *client_thread(void *arg)
 			printf("rx len:%d\n", readBufSize); //read()함수는 정상적으로 읽었다면 읽은 BYTE수를 반환 한다. 
 
 			rcv_buf[readBufSize] = 0;	//맨 뒷자리를 0으로 셋팅  
-			printf("%s\n", rcv_buf);	//읽어온 대로 출력  
+			printf("%s\n", rcv_buf);	//읽어온 대로 출력 
+			 
+			fprintf(stream,"%s", rcv_buf);	//파일 입력 ----------------------------수정 ============
 			
 			// echo TX
 			writeBufSize = write(clnt_sock,rcv_buf , readBufSize);  //받은 데이터를 clnt_sock으로 되돌려 줍니다. 
